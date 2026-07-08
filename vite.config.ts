7cd7112011f0994c922d4d71b94e0bd24d1d87ca
Phoenix Solutions <phoenixsudp@gmail.com>
2026-07-08 11:57:45 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PS_Websit/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
